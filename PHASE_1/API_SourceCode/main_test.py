13ce60b12020d286107595b3e1d5d1b9f102976d
FionaCLin <fiona.lin@student.unsw.edu.au>
2019-03-18 19:27:04 +1100

    Add key_terms tests

--- PHASE_1/API_SourceCode/main_test.py
+++ PHASE_1/API_SourceCode/main_test.py
@@ -13,12 +13,69 @@
 # limitations under the License.
 
 import main
+import json,os
+cwd = os.getcwd()
 
+sample_data = json.load(open(os.path.join(cwd,'../TestScripts/key-terms.json'),'r'))
 
+print(sample_data, 'ok')
+main.app.testing = True
+client = main.app.test_client()
+    
 def test_index():
-    main.app.testing = True
-    client = main.app.test_client()
-
     r = client.get('/')
     assert r.status_code == 200
-    assert 'Hello World' in r.data.decode('utf-8')
+
+def test_specific_key_terms():
+    main.app.testing = True
+    client = main.app.test_client()
+    r = client.get('/api/v1/reports/key-terms/specific')
+    assert r.status_code == 200
+    terms = json.loads(r.data.decode('utf-8'))
+    for e in terms:
+      print(e)
+      assert e['name'] in sample_data['specific']
+      assert e['category'] in ['a agent', 'none']
+      assert e['type'] == 'specific'
+
+def test_specific_w_category_key_terms():
+    main.app.testing = True
+    client = main.app.test_client()
+    r = client.get('/api/v1/reports/key-terms/specific?category=none')#<-- Update the url
+    assert r.status_code == 200
+    terms = json.loads(r.data.decode('utf-8'))
+    for e in terms:
+      print(e)
+      assert e['name'] in sample_data['specific']
+      assert e['category'] in ['a agent', 'none']
+      assert e['type'] == 'specific'
+
+
+def test_general_key_terms():
+  r = client.get('/api/v1/reports/key-terms/general')
+  assert r.status_code == 200
+  terms = json.loads(r.data.decode('utf-8'))
+  for e in terms:
+    print(e)
+    assert e['name'] in sample_data['general']
+    assert e['category'] == 'none'
+    assert e['type'] =='general'
+
+def  test_invalid_param():
+  r = client.get('/api/v1/reports/key-terms/xxx')
+  assert r.status_code == 400
+  r = client.get('/api/v1/reports/key-terms/')
+  assert r.status_code == 404
+  r = client.get('/api/v1/reports/key-terms/specific?caterogy=ccc')#<<< is this a invalid category?? it did not handler properly
+  assert r.status_code == 404
+   
+
+def test_key_terms():
+  test_invalid_param()
+  test_general_key_terms()
+  test_specific_key_terms()
+  test_specific_w_category_key_terms()
+
+
+test_index()
+test_key_terms()
