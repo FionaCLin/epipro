821167d689223f3d00795ca0c40646ce6bc8b643
ines219 <ines.sarmiento1@gmail.com>
2019-03-22 13:09:12 +1100

    latest version of typescripted front-end

---
+++ FlaskAndReact/flask-backend/static/react/precache-manifest.8c74a71690f87d880e350c17b1e6e06f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf63f4693246bf4d0507c4430961e21d",
+    "url": "/static/react/media/logo.bf63f469.png"
+  },
+  {
+    "revision": "867ae83af37611438a2b59e6b37b1e35",
+    "url": "/static/react/media/bg.867ae83a.jpg"
+  },
+  {
+    "revision": "450a472931a815e8e4c5",
+    "url": "/static/react/js/runtime~main.9d44af5e.js"
+  },
+  {
+    "revision": "042b9a2dc5d8ae247c9c",
+    "url": "/static/react/js/main.77a66a2e.chunk.js"
+  },
+  {
+    "revision": "ecfd1a3b8901a931525a",
+    "url": "/static/react/js/2.c27e67d0.chunk.js"
+  },
+  {
+    "revision": "042b9a2dc5d8ae247c9c",
+    "url": "/static/react/css/main.45201ce7.chunk.css"
+  },
+  {
+    "revision": "ecfd1a3b8901a931525a",
+    "url": "/static/react/css/2.c8e2e444.chunk.css"
+  },
+  {
+    "revision": "b1c9483f6717ba0ec044cf2a4a030ec8",
+    "url": "/static/react/../../templates/index.html"
+  }
+];
