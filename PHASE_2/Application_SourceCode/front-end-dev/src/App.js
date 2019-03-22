8d0d07c3c91f43272b6b7c2bb578527d3c6aa22e
FionaCLin <fiona.lin@student.unsw.edu.au>
2019-03-23 07:34:40 +1100

    Add dev front-end

---
+++ PHASE_2/Application_SourceCode/front-end-dev/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './logo.png';
+import './App.css';
+// import * as API from './API'
+
+class App extends Component {
+  render() {
+    let api_doc_url = window.token
+    return (
+      <div className="App">
+        <div className="bg">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p><strong>EpiPro API</strong></p>
+            <p>Predict. Prevent. Protect.</p>
+            <a className="a" href={api_doc_url}>
+              EpiPro API Documentation
+            </a>
+          </header>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
