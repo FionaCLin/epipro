1986af523d3df6decdd7c9c74be00b0fbcf3979b
FionaCLin <fiona.lin@student.unsw.edu.au>
2019-03-27 21:43:44 +1100

    Add encrypted config file and update beta test script

--- PHASE_1/TestScripts/e2e_test.py
+++ PHASE_1/TestScripts/e2e_test.py
@@ -13,7 +13,7 @@ import logging
 import json as json
 import sys
 
-HOST='https://'+sys.argv[1]
+HOST='https://beta-dot-epiproapp.appspot.com'
 
 # [START e2e]
 response = request.urlopen("{}/api/v1/swagger.json".format(HOST))
