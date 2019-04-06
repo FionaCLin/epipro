68eec9624d115a054ad357ccd9cf24214d628705
ines219 <ines.sarmiento1@gmail.com>
2019-04-06 19:19:20 +1100

    fixed imports

--- PHASE_2/Application_SourceCode/src/components/KeytermSearch.tsx
+++ PHASE_2/Application_SourceCode/src/components/KeytermSearch.tsx
@@ -2,115 +2,107 @@ import React from 'react';
 import '../css/Home.css';
 import Select from 'react-select';
 import CreatableSelect from 'react-select/lib/Creatable';
-import Keyterms from '../dummydata/keyterms.json';
 import { string } from 'prop-types';
 import { BackendAPI } from '../API'
 import { makeCreatableSelect } from 'react-select/lib/Creatable';
 
 let api = new BackendAPI();
+const filterTypes: Array<any> = [
+    { label: 'General', value: 0, type: 'general' },
+    { label: 'Specific', value: 1, type: 'specific' }
+];
 
 export default class KeytermSearch extends React.Component<IKeytermSearchProps, IKeytermSearchState> {
-  constructor(props: IKeytermSearchProps) {
-    super(props);
+    constructor(props: IKeytermSearchProps) {
+        super(props);
 
-    let keyterms: Array<any> = Keyterms.map((keyterm, index) => ({
-        label: keyterm.name,
-        value: index,
-        type: keyterm.type
-    }))
-
-    this.state = {
-        filterType: 0,
-        values: [],
-        filterOptions: keyterms,
-        keyterms
+        this.state = {
+            filterType: 0,
+            values: this.props.keyterms,
+            filterOptions: [],
+            keyterms: []
+        }
     }
-  }
-  
-  private handleChange(event: Array<any>) {
-    let values: Array<Number> = event.map(option => (option.value));
-    this.setState({values});
-    this.props.updateKeyterm(({keyterms: values}));
-  }
 
-  private changeType(event: any, filterTypes: Array<any>) {
-      this.setState({filterType: event.value});
-      if (event.value == 0) {
-          this.setState({filterOptions: this.state.keyterms});
-      } else {
-          this.setState({filterOptions: this.state.keyterms.filter(keyterm =>
-            (keyterm.type === filterTypes[event.value].type)
-            )});
-      }
-  }
+    componentWillMount() {
+        this.addFilterOptions(filterTypes[1]);
+        this.addFilterOptions(filterTypes[0]);
+        this.changeType(filterTypes[0]);
+    }
 
-  render() {
-    let key_terms;
-    api.getKeyTerms('general', (error: any, response: any) => {
-        if (error) {
-            if (error.response) {
+    private handleChange(event: Array<any>) {
+        console.log(event);
+        let values: Array<String> = event.map(option => (option.label));
+        this.setState({ values });
+        this.props.updateKeyterm(({ keyterms: values }));
+    }
+
+    private addFilterOptions(filterType: any) {
+        api.getKeyTerms(filterType.type, (error: any, response: any) => {
+            if (error && error.response) {
                 let message = error.response.data.message
-                console.log(message, 'ppp');
-            } else {
-                console.log(error.message, 'ppp');
+                console.log('error message', message);
+            } else if (error) {
+                console.log('error message', error.message);
             }
-        }
-        key_terms = response;
-        console.log(key_terms, 'general key term in keyterm search tsx')
-    });
 
-    api.getKeyTerms('specific', (error: any, response: any) => {
-        if (error) {
-            if (error.response) {
-                let message = error.response.data.message
-                console.log(message, 'ppp');
-            } else {
-                console.log(error.message, 'ppp');
-            }
-        }
-        key_terms = response;
-        console.log(key_terms, 'specific key term in keyterm search tsx')
-    });
+            let newKeyterms: Array<any> = response.map((keyterm: any) => ({
+                label: keyterm.name,
+                value: keyterm.name,
+                type: filterType.type
+            }));
 
-    let filterTypes: Array<any> = [
-        { label: 'All', value: 0, type: 'all' },
-        { label: 'General', value: 1, type: 'general' },
-        { label: 'Specific', value: 2, type: 'specific' }
-    ]   
-    return (
-        <div className="Filter-element">
-            <b>Keyterms</b>
-            <div className='Keyterm-flex'>
-                <div className='Keyterm-left'>
-                    <Select
-                        value={filterTypes.filter(type => type.value == this.state.filterType)}
-                        options={filterTypes}
-                        onChange={(e:any) => this.changeType(e, filterTypes)}
-                    />
-                </div>
-                <div className='Keyterm-right'>
-                    <CreatableSelect
-                        isMulti
-                        options={this.state.filterOptions}
-                        className="basic-multi-select"
-                        classNamePrefix="select"
-                        placeholder="Select keyterms..."
-                        onChange={(e:any) => this.handleChange(e)}
-                    />
+            this.setState({
+                keyterms: this.state.keyterms.concat(newKeyterms),
+                filterOptions: newKeyterms
+            })
+        });
+    }
+
+    private changeType(event: any) {
+        this.setState({
+            filterType: event.value,
+            filterOptions: this.state.keyterms.filter((keyterm: any) => keyterm.type == event.type)
+        });
+    }
+
+    render() {
+        return (
+            <div className="Filter-element">
+                <b>Keyterms</b>
+                <div className='Keyterm-flex'>
+                    <div className='Keyterm-left'>
+                        <Select
+                            value={filterTypes.filter(type => type.value == this.state.filterType)}
+                            options={filterTypes}
+                            onChange={(e: any) => this.changeType(e)}
+                        />
+                    </div>
+                    <div className='Keyterm-right'>
+                        <CreatableSelect
+                            isMulti
+                            options={this.state.filterOptions}
+                            className="basic-multi-select"
+                            classNamePrefix="select"
+                            placeholder="Select keyterms..."
+                            onChange={(e: any) => this.handleChange(e)}
+                            value={this.state.values.map((value: String) => { return { label: value, value }})}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 interface IKeytermSearchProps {
+    keyterms: Array<String>;
     updateKeyterm: (event: object) => void;
 }
 
 interface IKeytermSearchState {
     filterType: Number;
-    values: Array<Number>;
+    values: Array<String>;
     filterOptions: Array<Object>;
-    keyterms: Array<any>;
+    keyterms: Array<Object>
 }
