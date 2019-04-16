24546515863cf6f6d06ff2a78d2d1be3b24e6c5c
FionaCLin <fiona.lin@student.unsw.edu.au>
2019-04-17 00:42:37 +1000

    Clean up move request to back end

--- PHASE_2/Application_SourceCode/front-end-dev/src/Twitter.tsx
+++ PHASE_2/Application_SourceCode/front-end-dev/src/Twitter.tsx
@@ -9,61 +9,35 @@ export interface IFilterOptions {
 }
 
 export default class TwitterAPI {
-    baseURL: string = 'https://api.twitter.com/1.1/';
-    token: string = 'AAAAAAAAAAAAAAAAAAAAAE4u9wAAAAAAFfm2OtiR18Mpkrhod2Qif0oBYWg%3DgQblAjXSIAWNZA38v2g0MqjBolRnn8LC98PnVqe7WLKYExM0Yy';
-
-    client = new twitter({
-        consumer_key: 'UZM9rSwo2rQ6aukC5jA9kHdZ6',
-        consumer_secret: '6LpQcaJLjL6NsMYp6FNxmRPYP76iRQCDxvMGnw2tnti5zboiB7',
-        // access_token_key: '1114466561274683395-lh2q9hVstPaUyU3mfHxelC35Ro3UFc',
-        // access_token_secret: 'mSMj5aQZs84k6s7DGlKqvDyjCd0e9bLpkH5RHU9B5m6h9'
-        bearer_token: 'AAAAAAAAAAAAAAAAAAAAAE4u9wAAAAAAFfm2OtiR18Mpkrhod2Qif0oBYWg%3DgQblAjXSIAWNZA38v2g0MqjBolRnn8LC98PnVqe7WLKYExM0Yy'
-    });
-
     formatTwitterDate(date: string) {
         let newDate: string = date.replace(/T|-|:/g, '');
         return newDate.slice(0, newDate.length - 2);
     }
 
     getFilteredMedia(filter: IFilterOptions, cb:(err: any, res: any) => any) {
-        let q: any = {};
+        let payload: any = {};
         if (filter.disease) {
-            q['query'] = `is:verified ${filter.disease}`;
+            payload['query'] = `is:verified ${filter.disease}`;
             if (filter.location) {
-                q['query'] += ` ${filter.location}`;
+                payload['query'] += ` ${filter.location}`;
             }
         }
         if (filter.startDate) {
-            q['fromDate'] = this.formatTwitterDate(filter.startDate);
+            payload['fromDate'] = this.formatTwitterDate(filter.startDate);
         }
         if (filter.endDate) {
-            q['toDate'] = this.formatTwitterDate(filter.endDate);
+            payload['toDate'] = this.formatTwitterDate(filter.endDate);
         }
-
-        if (Object.keys(q).length != 0) {
-            let config = {
-                headers: {
-                    request: 'POST',
-                    authorization: `Bearer ${this.token}`,
-                    'content-type': 'application/json',
-                    'Access-Control-Allow-Origin': 'http://127.0.0.1:3000',
-                    'Access-Control-Allow-Methods': "DELETE, POST, GET, OPTIONS",
-                    'Access-Control-Allow-Headers': "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"
-                },
-                params: q
-            }
-            let url =  `${this.baseURL}search/30day/development.json`;
-            axios.get(url, config)
+        
+        if (Object.keys(payload).length != 0) {
+            axios.post('http://localhost:8080/api/v1/twitter',payload)
                 .then((response: AxiosResponse) => {
-                    console.log(response.data);
+                    // console.log(response.data);
                     cb(null, response.data);
                 })
                 .catch((error: AxiosError) => {
                     cb(error, null)
             });
-            // this.client.get('search/30day/development', q, function(error, tweets, response) {
-            //     console.log(tweets);
-            // });
         }
     }
 }
