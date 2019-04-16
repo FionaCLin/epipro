c1602e79ba4f28f9522176f2e3e4855b1c24ff3c
Fiona Lin <fiona.lin@student.unsw.edu.au>
2019-04-16 20:44:11 +1000

    Remove unfinished front end

--- PHASE_2/Application_SourceCode/src/App.tsx
+++ PHASE_2/Application_SourceCode/src/App.tsx
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Home from './components/Home';
-import Search from './components/Search';
-import Article from './components/Article';
 import { BackendAPI } from './API'
-import Analytics from './components/Analytics';
 
 export default class App extends Component<IAppProps, IAppState> {
   constructor(props: IAppProps) {
@@ -17,9 +14,6 @@ export default class App extends Component<IAppProps, IAppState> {
         <Router>
           <Switch>
             <Route exact path="/" component={Home}></Route>
-            <Route path="/search" component={Search} />
-            <Route path="/article" component={Article} />
-            <Route path="/analyze" component={Analytics} />
             <Redirect to="/" />
           </Switch>
         </Router>
