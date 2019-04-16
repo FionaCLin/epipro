70988489d0c31f7c80b481bdbe534110b9c897f1
Fiona Lin <fiona.lin@student.unsw.edu.au>
2019-04-16 20:43:12 +1000

    Update front end

---
+++ PHASE_1/API_SourceCode/static/react/precache-manifest.7f612f1b1c3505dbcf58bcb33808e60a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf63f4693246bf4d0507c4430961e21d",
+    "url": "/static/react/media/logo.bf63f469.png"
+  },
+  {
+    "revision": "867ae83af37611438a2b59e6b37b1e35",
+    "url": "/static/react/media/bg.867ae83a.jpg"
+  },
+  {
+    "revision": "450a472931a815e8e4c5",
+    "url": "/static/react/js/runtime~main.450a4729.js"
+  },
+  {
+    "revision": "3ece1c1d4ba5d754dd2e",
+    "url": "/static/react/js/main.3ece1c1d.chunk.js"
+  },
+  {
+    "revision": "add9b6ea54b6a4c278a6",
+    "url": "/static/react/js/2.add9b6ea.chunk.js"
+  },
+  {
+    "revision": "3ece1c1d4ba5d754dd2e",
+    "url": "/static/react/css/main.30ebaf3b.chunk.css"
+  },
+  {
+    "revision": "add9b6ea54b6a4c278a6",
+    "url": "/static/react/css/2.50631302.chunk.css"
+  },
+  {
+    "revision": "561f056c2eb2867f4c6def3658cb51c2",
+    "url": "/static/react/../../templates/index.html"
+  }
+];
