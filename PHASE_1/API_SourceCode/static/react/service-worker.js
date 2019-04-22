58c427880b4a7b966a9d1935276b8afcdf4e046e
Fiona Lin <fiona.lin@student.unsw.edu.au>
2019-04-22 16:05:17 +1000

    Update first build

--- PHASE_1/API_SourceCode/static/react/service-worker.js
+++ PHASE_1/API_SourceCode/static/react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/static/react/precache-manifest.7f612f1b1c3505dbcf58bcb33808e60a.js"
+  "/static/react/precache-manifest.3e3f455043497e7cfdf398c93a791fe0.js"
 );
 
 workbox.clientsClaim();
